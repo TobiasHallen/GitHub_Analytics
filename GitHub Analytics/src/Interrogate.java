22fb2f0495d667ee9a8f521282e76227219caccc
Tobias Hallen <32342223+TobiasHallen@users.noreply.github.com>
2018-11-10 19:59:37 +0000

    Adding gson and egit libraries to interact with github api, added test to print repository of dummy user

--- GitHub Analytics/src/Interrogate.java
+++ GitHub Analytics/src/Interrogate.java
@@ -1,9 +1,21 @@
+import java.io.IOException;
+
+import org.eclipse.egit.github.core.*;
+import com.google.gson.*;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class Interrogate 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		System.out.println("Helloworld");
+		GitHubClient client = new GitHubClient();
+		client.setCredentials("GithubTesteroni", "examplepassword123");
+
+		RepositoryService service = new RepositoryService();
+		for (Repository repo : service.getRepositories("GithubTesteroni"))
+		  System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
+
 	}
 	
 }
